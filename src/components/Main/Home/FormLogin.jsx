b3b0562d92b36f390bde647d8e3666e313646303
GonzaloRobledo <gonza_roble99@hotmail.com>
2023-08-19 00:02:44 -0300

    update

--- src/components/Main/Home/FormLogin.jsx
+++ src/components/Main/Home/FormLogin.jsx
@@ -1,8 +1,11 @@
+import {Link } from 'react-router';
+
 export const FormLogin = () => {
     return <form className="form-login">
         <h3>Login</h3>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <input type="submit" value="Login" />
+        <Link to="/pending">Go pending</Link>
    </form>
 }
